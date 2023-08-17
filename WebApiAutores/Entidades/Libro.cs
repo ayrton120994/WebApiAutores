3ea63e2645e9b5fe6a70d23c976f8c8ff64438e8
Ayrton La Madrid <ayrtoncesar12@gmail.com>
2023-08-17 15:40:05 -0500

    Relacion de uno a muchos

--- WebApiAutores/Entidades/Libro.cs
+++ WebApiAutores/Entidades/Libro.cs
@@ -6,9 +6,11 @@ namespace WebApiAutores.Entidades
     public class Libro
     {
         public int Id { get; set; }
+        [Required]
         [PrimeraLetraMayuscula]
         [StringLength(maximumLength: 250)]
         public string Titulo { get; set; }
+        public List<Comentario> Comentarios { get; set; }
         //public int AutorId { get; set; }
         //public Autor Autor { get; set; }
     }
