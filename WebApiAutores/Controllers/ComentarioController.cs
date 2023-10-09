13d7db5ce6d0b595c47a705fdf28fc4fc8f3fef2
Ayrton La Madrid <ayrtoncesar12@gmail.com>
2023-10-09 11:28:56 -0500

    Controlador de un Recurso Dependiente

---
+++ WebApiAutores/Controllers/ComentarioController.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiAutores.DTOs;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/libros/{libroId:int}/comentarios")]
+    public class ComentarioController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public ComentarioController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ComentarioDTO>>> Get(int libroId)
+        {
+            var existeLibro = await _context.Libros.AnyAsync(libroDB => libroDB.Id == libroId);
+
+            if (!existeLibro)
+            {
+                return NotFound();
+            }
+
+            var comentarios = await _context.Comentarios
+                .Where(comentarioDB => comentarioDB.LibroId == libroId).ToListAsync();
+
+            return _mapper.Map<List<ComentarioDTO>>(comentarios);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(int libroId, ComentarioCreacionDTO comentarioCreacionDTO)
+        {
+            var existeLibro = await _context.Libros.AnyAsync(libroDB => libroDB.Id == libroId);
+
+            if (!existeLibro)
+            {
+                return NotFound();
+            }
+
+            var comentario = _mapper.Map<Comentario>(comentarioCreacionDTO);
+            comentario.LibroId = libroId;
+
+            _context.Add(comentario);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
