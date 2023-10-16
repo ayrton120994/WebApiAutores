67edc6631a2489305f48c2cb63f4407d56358b64
Ayrton La Madrid <ayrtoncesar12@gmail.com>
2023-10-16 11:44:11 -0500

    Relacion muchos a muchos

--- WebApiAutores/Entidades/Autor.cs
+++ WebApiAutores/Entidades/Autor.cs
@@ -12,6 +12,7 @@ namespace WebApiAutores.Entidades
         [StringLength(maximumLength: 120, ErrorMessage = "El campo {0} no debe tener más de {1} carácteres")]
         [PrimeraLetraMayuscula]
         public string Nombre { get; set; }
+        public List<AutorLibro> AutoresLibros { get; set; }
 
         #region Ejemplo - Reglas de Validacion
         //[Range(18, 120)]
