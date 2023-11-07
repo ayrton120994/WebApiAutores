18e2e35a9d773f56dc3d6a27b0306926454e0905
Ayrton La Madrid <ayrtoncesar12@gmail.com>
2023-11-06 23:03:15 -0500

    Configurando la autorización basada en Claims

--- WebApiAutores/Startup.cs
+++ WebApiAutores/Startup.cs
@@ -81,6 +81,11 @@ namespace WebApiAutores
             services.AddIdentity<IdentityUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
+
+            services.AddAuthorization(opciones =>
+            {
+                opciones.AddPolicy("EsAdmin", politica => politica.RequireClaim("esAdmin"));
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
